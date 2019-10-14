58fa303d1c9480910511949603b04ee3b266e8fb
Tanuraj <tanuraj@darkbears.com>
2019-10-14 12:50:46 +0530

    Get auth user profile

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -4,14 +4,23 @@ const auth = require('../../middleware/auth');
 
 //Use models
 const Profile = require('../../models/Profile');
+const User = require('../../models/User');
 
-// @route   GET api/profile
-// @desc    Test route
+// @route   GET api/profile/me
+// @desc    Get current user profile
 // @access  Private
-router.get('/', auth, async (req, res) => {
+router.get('/me', auth, async (req, res) => {
     //Find user auth user data
     try {
-        const profile = await Profile.findOne({ user_id: req.user.id });
+        const profile = await Profile.findOne({ user: req.user.id }).populate(
+            'user',
+            ['name', 'image']
+        );
+
+        //Check data
+        if (!profile) {
+            return res.status(400).json({ msg: 'No profile found.' });
+        }
         res.json(profile);
     } catch (err) {
         console.log(err.message);
