7fdc5aeff7738a5d5bcbca496440d65a348f22c4
Tanuraj <tanuraj@darkbears.com>
2019-10-15 13:10:21 +0530

    Create post model

---
+++ models/Post.js
@@ -0,0 +1,56 @@
+const mongoose = require('mongoose');
+
+//Create Schema
+const PostSchema = new mongoose.Schema({
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'users'
+    },
+    text: {
+        type: String,
+        required: true
+    },
+    name: {
+        type: String
+    },
+    image: {
+        type: String
+    },
+    likes: [
+        {
+            user: {
+                type: Schema.Types.ObjectId,
+                ref: 'users'
+            }
+        }
+    ],
+    comments: [
+        {
+            user: {
+                type: Schema.Types.ObjectId,
+                ref: 'users'
+            },
+            text: {
+                type: String,
+                required: true
+            },
+            name: {
+                type: String
+            },
+            image: {
+                type: String
+            },
+            date: {
+                type: Date,
+                default: Date.now
+            }
+        }
+    ],
+    date: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+//Export model
+module.exports = Post = mongoose.model('post', PostSchema);
