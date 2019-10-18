22cef3a53ee2f9fc3684eb240c3a5a4fb0a1d573
Tanuraj <tanuraj@darkbears.com>
2019-10-18 12:02:41 +0530

    Create login/ registration form

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -1,7 +1,60 @@
-import React from 'react';
+import React, { Fragment, useState } from 'react';
+import { Link } from 'react-router-dom';
 
 const Login = () => {
-	return <div>Login</div>;
+	const [formData, setFormData] = useState({
+		email: '',
+		password: ''
+	});
+
+	const { email, password } = formData;
+
+	const onChangeHandler = e =>
+		setFormData({ ...formData, [e.target.name]: e.target.value });
+
+	const onSubmitHandler = async e => {
+		e.preventDefault();
+		console.log('Success');
+	};
+
+	return (
+		<Fragment>
+			<h1 className='large text-primary'>Sign In</h1>
+			<p className='lead'>
+				<i className='fa fa-user'></i> Sign into Your Account
+			</p>
+			<form className='form' onSubmit={e => onSubmitHandler(e)}>
+				<div className='form-group'>
+					<input
+						type='email'
+						placeholder='Email Address'
+						name='email'
+						value={email}
+						onChange={e => onChangeHandler(e)}
+					/>
+				</div>
+				<div className='form-group'>
+					<input
+						type='password'
+						placeholder='Password'
+						name='password'
+						minLength='6'
+						value={password}
+						onChange={e => onChangeHandler(e)}
+					/>
+				</div>
+
+				<input
+					type='submit'
+					className='btn btn-primary'
+					value='Login'
+				/>
+			</form>
+			<p className='my-1'>
+				Already have an account? <Link to='/register'>Sign Up</Link>
+			</p>
+		</Fragment>
+	);
 };
 
 export default Login;
--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,5 +1,5 @@
 import React, { Fragment, useState } from 'react';
-import axios from 'axios';
+import { Link } from 'react-router-dom';
 
 const Register = () => {
 	const [formData, setFormData] = useState({
@@ -21,22 +21,7 @@ const Register = () => {
 		if (password !== password2) {
 			console.log('Passwords do not change');
 		} else {
-			const newUser = { name, email, password };
-
-			try {
-				const config = {
-					headers: {
-						'Content-Type': 'application/json'
-					}
-				};
-
-				const body = JSON.stringify(newUser);
-
-				const res = await axios.post('/api/users', body, config);
-				console.log(res.data);
-			} catch (err) {
-				console.error(err.response.data);
-			}
+			console.log('Success');
 		}
 	};
 
@@ -97,7 +82,7 @@ const Register = () => {
 				/>
 			</form>
 			<p className='my-1'>
-				Already have an account? <a href='login.html'>Sign In</a>
+				Already have an account? <Link to='/login'>Sign In</Link>
 			</p>
 		</Fragment>
 	);
