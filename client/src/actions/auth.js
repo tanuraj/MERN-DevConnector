c312f1ad4c241caa98ba7fb3c95f02dcb2e3d568
Tanuraj <tanuraj@darkbears.com>
2019-10-21 19:00:18 +0530

    user login

--- client/src/actions/auth.js
+++ client/src/actions/auth.js
@@ -3,39 +3,12 @@ import {
 	REGISTER_SUCCESS,
 	REGISTER_FAIL,
 	USER_LOADED,
-	AUTH_ERROR
+	AUTH_ERROR,
+	LOGIN_FAIL,
+	LOGIN_SUCCESS
 } from './types';
 import { setAlert } from './alert';
 import setAuthToken from '../utils/setAuthToken';
-
-//Register user
-export const register = ({ name, email, password }) => async dispatch => {
-	const config = {
-		headers: {
-			'Content-Type': 'application/json'
-		}
-	};
-
-	const body = JSON.stringify({ name, email, password });
-
-	try {
-		const res = await axios.post('/api/users', body, config);
-		dispatch({
-			type: REGISTER_SUCCESS,
-			payload: res.data
-		});
-	} catch (err) {
-		const errors = err.response.data.errors;
-
-		if (errors) {
-			errors.forEach(error => dispatch(setAlert(error.msg, 'danger')));
-		}
-
-		dispatch({
-			type: REGISTER_FAIL
-		});
-	}
-};
 
 //Load User
 export const loadUser = () => async dispatch => {
@@ -53,6 +26,67 @@ export const loadUser = () => async dispatch => {
 	} catch (error) {
 		dispatch({
 			type: AUTH_ERROR
+		});
+	}
+};
+
+//Register user
+export const register = ({ name, email, password }) => async dispatch => {
+	const config = {
+		headers: {
+			'Content-Type': 'application/json'
+		}
+	};
+
+	const body = JSON.stringify({ name, email, password });
+
+	try {
+		const res = await axios.post('/api/users', body, config);
+		dispatch({
+			type: REGISTER_SUCCESS,
+			payload: res.data
+		});
+		dispatch(loadUser());
+	} catch (err) {
+		const errors = err.response.data.errors;
+
+		if (errors) {
+			errors.forEach(error => dispatch(setAlert(error.msg, 'danger')));
+		}
+
+		dispatch({
+			type: REGISTER_FAIL
+		});
+	}
+};
+
+//Login user
+export const login = (email, password) => async dispatch => {
+	const config = {
+		headers: {
+			'Content-Type': 'application/json'
+		}
+	};
+
+	const body = JSON.stringify({ email, password });
+
+	try {
+		const res = await axios.post('/api/auth', body, config);
+		dispatch({
+			type: LOGIN_SUCCESS,
+			payload: res.data
+		});
+
+		dispatch(loadUser());
+	} catch (err) {
+		const errors = err.response.data.errors;
+
+		if (errors) {
+			errors.forEach(error => dispatch(setAlert(error.msg, 'danger')));
+		}
+
+		dispatch({
+			type: LOGIN_FAIL
 		});
 	}
 };
--- routes/api/auth.js
+++ routes/api/auth.js
@@ -11,85 +11,84 @@ const bcrypt = require('bcryptjs');
 // @desc    Test route
 // @access  Public
 router.get('/', auth, async (req, res) => {
-    //Find user auth user data
-    try {
-        const user = await User.findById(req.user.id).select('-password');
-        res.json(user);
-    } catch (err) {
-        console.log(err.message);
-        res.status(500).send('Server error');
-    }
+	//Find user auth user data
+	try {
+		const user = await User.findById(req.user.id).select('-password');
+		res.json(user);
+	} catch (err) {
+		console.log(err.message);
+		res.status(500).send('Server error');
+	}
 });
 
 // @route   POST api/auth
 // @desc    Authenticate User & get token (login)
 // @access  Public
 router.post(
-    '/',
-    [
-        //chaeck not empty validation
-        check('email', 'Please enter valid email').isEmail(),
-        check('password', 'Please enter valid password').exists()
-    ],
-    async (req, res) => {
-        //Check validattion error
-        const errors = validationResult(req);
-        if (!errors.isEmpty()) {
-            return res.status(400).json({ errors: errors.array() });
-        }
+	'/',
+	[
+		//chaeck not empty validation
+		check('email', 'Please enter valid email').isEmail(),
+		check('password', 'Please enter valid password').exists()
+	],
+	async (req, res) => {
+		//Check validattion error
+		const errors = validationResult(req);
+		if (!errors.isEmpty()) {
+			return res.status(400).json({ errors: errors.array() });
+		}
 
-        //Assign variable with req body
-        const { email, password } = req.body;
+		//Assign variable with req body
+		const { email, password } = req.body;
 
-        try {
-            /*  Query always return a promise 
+		try {
+			/*  Query always return a promise 
                 which was handle by .then().catch()
                 by using await we handle this 
                 For function which return promise we async/await for clear code    
             */
-            let user = await User.findOne({ email });
+			let user = await User.findOne({ email });
 
-            //See if user exists
-            if (!user) {
-                return res
-                    .status(400)
-                    .send({ errors: [{ msg: 'Invalid credentials' }] });
-            }
+			//See if user exists
+			if (!user) {
+				return res
+					.status(400)
+					.send({ errors: [{ msg: 'Invalid credentials' }] });
+			}
 
-            //Match user credentials
-            const isMatch = bcrypt.compare(password, user.password);
+			//Match user credentials
+			const isMatch = await bcrypt.compare(password, user.password);
+			if (!isMatch) {
+				return res
+					.status(400)
+					.send({ errors: [{ msg: 'Invalid credentials' }] });
+			}
 
-            if (!isMatch) {
-                return res
-                    .status(400)
-                    .send({ errors: [{ msg: 'Invalid credentials' }] });
-            }
+			//Return jsonwebtoken start
+			//create payload
+			const payload = {
+				user: {
+					id: user.id
+				}
+			};
 
-            //Return jsonwebtoken start
-            //create payload
-            const payload = {
-                user: {
-                    id: user.id
-                }
-            };
-
-            //jwt signin
-            jwt.sign(
-                payload,
-                config.get('jwtSecret'),
-                {
-                    expiresIn: 360000 //ideal expire time is 3600
-                },
-                (err, token) => {
-                    if (err) throw err;
-                    res.json({ token });
-                }
-            );
-        } catch (err) {
-            console.log(err.message);
-            return res.status(500).send('Server Error');
-        }
-    }
+			//jwt signin
+			jwt.sign(
+				payload,
+				config.get('jwtSecret'),
+				{
+					expiresIn: 360000 //ideal expire time is 3600
+				},
+				(err, token) => {
+					if (err) throw err;
+					res.json({ token });
+				}
+			);
+		} catch (err) {
+			console.log(err.message);
+			return res.status(500).send('Server Error');
+		}
+	}
 );
 
 module.exports = router;
