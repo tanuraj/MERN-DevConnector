4fd537da7d3c4bfec9a53f3d18335ff0b2bcc629
Tanuraj <tanuraj@darkbears.com>
2019-10-22 14:47:32 +0530

    Setup privaate route

---
+++ client/src/components/dashboard/Dashboard.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+export class Dashboard extends Component {
+	static propTypes = {};
+
+	render() {
+		return <div>Dashboard</div>;
+	}
+}
+export default Dashboard;
