3604f8b9774e04c467b088dd6d681e746c913717
Tanuraj <tanuraj@darkbears.com>
2019-10-24 11:01:09 +0530

    Create profile form

--- client/src/App.js
+++ client/src/App.js
@@ -7,6 +7,7 @@ import Login from './components/auth/Login';
 import Alert from './components/layout/Alert';
 import Dashboard from './components/dashboard/Dashboard';
 import PrivateRoute from './components/routing/PrivateRoute';
+import CreateProfile from './components/profile-forms/CreateProfile';
 
 //Redux
 import { Provider } from 'react-redux';
@@ -44,6 +45,11 @@ const App = () => {
 								exact
 								path='/dashboard'
 								component={Dashboard}
+							/>
+							<PrivateRoute
+								exact
+								path='/create-profile'
+								component={CreateProfile}
 							/>
 						</Switch>
 					</section>
